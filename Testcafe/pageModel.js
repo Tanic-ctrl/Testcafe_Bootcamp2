bf6aaa4e7dd817de8ca55af00cf989d218fdad41
“Tania” <tania@photoshelter.com>
2020-10-17 12:59:09 -0500

    Agregar practicas 2 a 4

--- Testcafe/pageModel.js
+++ Testcafe/pageModel.js
@@ -2,8 +2,34 @@ import { Selector } from 'testcafe';
 
 class Page {
     constructor() {
+        //Practica 2, no es conveniente tener nombre de selectores muy largo
+        // this.link2 = Selector('#content > ul > li:nth-child(1) > a');
 
+        //Practica 2, mejor práctica
+        this.link2 = Selector('a').withText('A/B Testing')
+            //this.text21 = Selector('h3').with('A/B Test Control')
+        this.text21 = Selector('#content > div > h3')
+
+        //Practica 3, Forgot password
+        this.link3 = Selector('a').withText('Forgot Password')
+        this.input31 = Selector('#email')
+        this.button32 = Selector('#form_submit')
+        this.text33 = Selector('#content')
+
+        //Práctica 4, checkboxes
+        this.link4 = selector('a').withText('Checkboxes')
+            //Checkbox Padre
+            // this.baseCheckbox = selector('#checkboxes > input [type=checkbox]')
+            //this.checkbox41 = Selector('#checkboxes > input[type=checkbox]:nth-child(1)');    
+            //this.checkbox41 = Selector('#checkboxes > input[type=checkbox]:nth-child(3)');
+
+        // Practica 4 Seida
+        this.link4 = Selector('a').withText('Checkboxes')
+            //Checkbox Padre
+        this.baseCheckbox = Selector('#checkboxes > input[type=checkbox]')
+            //Checkboxs Hijos
+            //this.checkbox41 = Selector('#checkboxes > input[type=checkbox]:nth-child(1)');
+        this.checkbox42 = Selector('#checkboxes > input[type=checkbox]:nth-child(3)');
     }
 }
-
-export default new Page()
+export default new Page();
--- Testcafe/practica2.js
+++ Testcafe/practica2.js
@@ -5,5 +5,13 @@ fixture('Práctica 2 con testcafe')
     .page('https://the-internet.herokuapp.com/');
 
 test('Probar dar clic en un enlace', async t => {
+    await t
+        .click(page.link2)
+    await t
 
+    if (page.text21.innerText == "A/B Test Control" || "A/B Test Variation 1" || "A/B Test Variation") {
+        console.log("Pasa prueba")
+    } else {
+        console.log("No pasa")
+    }
 });
