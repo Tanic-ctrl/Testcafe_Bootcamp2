fcbc567fc7f25409ab55b4af1b9e65b5b59825a9
“Tania” <tania@photoshelter.com>
2020-10-27 21:27:01 -0600

    Agregar data file

---
+++ E-commerce/data.js
@@ -0,0 +1,12 @@
+let rndNumber = Math.random().toString(36).substr(1, 4);
+
+
+let data = {
+    email: 'dummy' + rndNumber + '@mailinator.com',
+    firstName: 'Tania',
+    lastName: 'Radilla',
+    password: '99999',
+    pbox: "PO Box 7687",
+    city: "New York",
+}
+export { data }
